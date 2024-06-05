5712f0e0c5e725487955a6425c884a2fe2a3c7e0
Sawatdikiat Saengboon <11432986@krungsri.com>
2024-06-05 08:49:36 +0700

    Initial to git

---
+++ android/app/src/main/kotlin/com/example/study_flutter_meals_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.study_flutter_meals_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'package:study_flutter_meals_app/models/meal.dart';
+import 'package:study_flutter_meals_app/screens/meal_details_screen.dart';
+import 'package:study_flutter_meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
+
+  final String? title;
+  final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here.',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (context, meal) => selectMeal(context, meal),
+        ),
+      );
+    }
+    if (title == null) {
+      return content;
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
